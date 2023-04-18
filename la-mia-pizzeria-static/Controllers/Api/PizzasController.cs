545581b232e6a9a79a0e8b1588bf32127ce3b9e7
Elsti97 <elsti.shehati@hotmail.it>
2023-04-18 14:57:42 +0200

    POST/PUT/DELETE Done

--- la-mia-pizzeria-static/Controllers/Api/PizzasController.cs
+++ la-mia-pizzeria-static/Controllers/Api/PizzasController.cs
@@ -35,5 +35,50 @@ namespace la_mia_pizzeria_static.Controllers.Api
             }
             return Ok(pizzaSingola);
         }
+
+        [HttpPost]
+        public IActionResult CreatePizza([FromBody] Pizza pizza)
+        {
+            _context.Pizzas.Add(pizza);
+            _context.SaveChanges();
+
+            return Ok(pizza);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePizza(int id, [FromBody] Pizza pizza)
+        {
+            var updatePizza = _context.Pizzas.FirstOrDefault(p => p.Id == id);
+
+            if (updatePizza == null)
+            {
+                return NotFound();
+            }
+
+            updatePizza.Nome = pizza.Nome;
+            updatePizza.Descrizione = pizza.Descrizione;
+            updatePizza.Prezzo = pizza.Prezzo;
+            updatePizza.CategoryId = pizza.CategoryId;
+
+            _context.SaveChanges();
+
+            return Ok(updatePizza);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletePizza(int id)
+        {
+            var pizza = _context.Pizzas.FirstOrDefault(p => p.Id == id);
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pizzas.Remove(pizza);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
